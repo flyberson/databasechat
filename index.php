0d6285e7f8c662fb5dbc5c664d196eb8461fa5c6
flyberson <flyberson12@gmail.com>
2017-09-27 17:02:56 +0200

    something

--- index.php
+++ index.php
@@ -90,6 +90,12 @@ if ($result->num_rows > 0) {
 ?>
 
 </table>
+
+<form action="index.php" method="post">
+    Name: <input type="text" name="name"><br>
+    E-mail: <input type="text" name="email"><br>
+    <input type="submit">
+</form>
+
 </body>
 </html>
-
